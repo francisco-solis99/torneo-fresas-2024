1d9c7ef17fa54313055bad0a4a61493b1f41e93b
francisco-solis99 <franciscosoliscss@gmail.com>
2024-03-21 16:44:16 -0600

    [chore] add the ui for the match dialog using shadcn componets

---
+++ src/components/MatchDialog.tsx
@@ -0,0 +1,59 @@
+import { Button } from "@/components/ui/button"
+import { MoreHorizontal } from "lucide-react"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export default function MatchDialog() {
+  return (
+    <div className="absolute -top-10 right-0">
+      <Dialog>
+        {/* Dropdown */}
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Match</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            {/* Options */}
+            <DialogTrigger asChild>
+              <DropdownMenuItem>
+                <span>Registrar score</span>
+              </DropdownMenuItem>
+            </DialogTrigger>
+            <DropdownMenuItem>Eliminar Match</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        {/* Dialog content */}
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. Are you sure you want to permanently
+              delete this file from our servers?
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  )
+}
