760f350af85c36f0e57672d68942baa77d0b2806
francisco-solis99 <franciscosoliscss@gmail.com>
2024-03-29 00:48:07 -0600

    [feat] add a function to gill teh froups with duos randomly

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -55,7 +55,7 @@ db.exec(`CREATE TABLE IF NOT EXISTS duos (
       ON UPDATE NO ACTION
 )`);
 
-// Seeds
+// Seeds users
 // const users = [{ username: "pat-mahomes", password: "KC03PLM15" }];
 
 // const queryInsert = db.prepare(
@@ -68,21 +68,132 @@ db.exec(`CREATE TABLE IF NOT EXISTS duos (
 //   queryInsert.run(userId, user.username, hashedPassword);
 // });
 
-// example using the better-sqlite3
-// Create a table with users
-// db.exec(`CREATE TABLE IF NOT EXISTS user (
-//   id TEXT NOT NULL PRIMARY KEY,
-//   username TEXT NOT NULL UNIQUE,
-//   hashed_password TEXT NOT NULL
-// )`);
+// Seeds groups
+// const groups = [
+//   {
+//     id: 1,
+//     name: "A",
+//   },
+//   {
+//     id: 2,
+//     name: "B",
+//   },
+//   {
+//     id: 3,
+//     name: "C",
+//   },
+// ];
+// const queryInsertGroups = db.prepare(
+//   "INSERT INTO groups (id, name) VALUES (?, ?)"
+// );
 
-// // Create a table with sessions with a relation to users table
-// db.exec(`CREATE TABLE IF NOT EXISTS session (
-//   id TEXT NOT NULL PRIMARY KEY,
-//   expires_at INTEGER NOT NULL,
-//   user_id TEXT NOT NULL,
-//   FOREIGN KEY (user_id) REFERENCES user(id)
-// )`);
+// groups.forEach((group) => {
+//   queryInsertGroups.run(group.id, group.name);
+// });
+
+// Seeds users
+// const users = [{ username: "pat-mahomes", password: "KC03PLM15" }];
+
+// const queryInsert = db.prepare(
+//   "INSERT INTO user (id, username, hashedPassword) VALUES (?, ?, ?)"
+// );
+
+// users.forEach(async (user) => {
+//   const userId = generateId(15);
+//   const hashedPassword = await new Argon2id().hash(user.password);
+//   queryInsert.run(userId, user.username, hashedPassword);
+// });
+
+// Seeds duos
+// const duos = [
+//   {
+//     player1: "Dak Prescott",
+//     player2: "Ceede Lamb",
+//   },
+//   {
+//     player1: "Jalen Hurts",
+//     player2: "Aj Brown",
+//   },
+//   {
+//     player1: "Brock Purdy",
+//     player2: "Brando Ayuk",
+//   },
+//   {
+//     player1: "Tom Brady",
+//     player2: "Rob Gronkoski",
+//   },
+//   {
+//     player1: "Kirk Cousins",
+//     player2: "Justin Jefferson",
+//   },
+//   {
+//     player1: "Patrick Mahomes",
+//     player2: "Travis Kelce",
+//   },
+//   {
+//     player1: "Tua Tagovailoa",
+//     player2: "Tyreek Hill",
+//   },
+//   {
+//     player1: "Josh Allen",
+//     player2: "Stephon Diggs",
+//   },
+//   {
+//     player1: "Joe Burrow",
+//     player2: "Jamar Chase",
+//   },
+//   {
+//     player1: "Lamar Jackson",
+//     player2: "Zay Flowers",
+//   },
+//   {
+//     player1: "Matthew Stafford",
+//     player2: "Puca Nakua",
+//   },
+//   {
+//     player1: "CJ Stroud",
+//     player2: "Nico Collins",
+//   },
+//   {
+//     player1: "Jared Goff",
+//     player2: "Amon-Ra St Brown",
+//   },
+//   {
+//     player1: "Justin Herbert",
+//     player2: "Keenan Allen",
+//   },
+//   {
+//     player1: "Baker Mayyield",
+//     player2: "Mike Evans",
+//   },
+//   {
+//     player1: "Jordan Love",
+//     player2: "Romeo Doubs",
+//   },
+//   {
+//     player1: "Aaron Rodgers",
+//     player2: "Garret Wilson",
+//   },
+//   {
+//     player1: "Russel Wilson",
+//     player2: "Courtland Sutton",
+//   },
+//   {
+//     player1: "Jimmy Garapollo",
+//     player2: "Davante Adams",
+//   },
+//   {
+//     player1: "Derek Car",
+//     player2: "Chris Olave",
+//   },
+// ];
+// const queryInsertDuos = db.prepare(
+//   "INSERT INTO duos (player1, player2) VALUES (?, ?)"
+// );
+
+// duos.forEach((duo) => {
+//   queryInsertDuos.run(duo.player1, duo.player2);
+// });
 
 // ------------------------------------ EXAMPLES WITH DOCS ------------------------------
 // INSERT DATA
@@ -115,3 +226,28 @@ db.exec(`CREATE TABLE IF NOT EXISTS duos (
  -> Run is when you dont expect a return value
  -> Use prepare when you expect some return value like when you use a SELECT
 */
+
+function fillDuosGroupRandomly() {
+  const randomDuos = db.prepare("SELECT * FROM duos ORDER BY random()").all();
+  const groupsId: any[] = db.prepare("SELECT id FROM groups").all();
+  const numDuosPerGroup = 3;
+
+  const updateDuoGroupId = db.prepare(
+    "UPDATE duos SET group_id = ? WHERE id = ?"
+  );
+  let indexGroup = 0;
+  randomDuos.forEach((duo: any, index: number) => {
+    if (index % numDuosPerGroup === 0) indexGroup += 1;
+    const groupId = groupsId[indexGroup - 1];
+    updateDuoGroupId.run(groupId.id, duo.id);
+  });
+}
+
+// fillDuosGroupRandomly();
+
+// const duosByGroup = db
+//   .prepare(
+//     "SELECT duos.id, player1, player2, group_id As groupId, groups.name AS group_name FROM duos LEFT JOIN groups ON groups.id = duos.group_id"
+//   )
+//   .all();
+// console.log(duosByGroup);
