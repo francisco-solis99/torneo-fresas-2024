77d3241932e102dbcdfe85410cb0d622322fb0f5
francisco-solis99 <franciscosoliscss@gmail.com>
2024-03-30 22:53:20 -0600

    [style] title and remove button

--- src/components/MatchDialog.tsx
+++ src/components/MatchDialog.tsx
@@ -24,10 +24,10 @@ export default function MatchDialog() {
         {/* Dropdown */}
         <DropdownMenu>
           <DropdownMenuTrigger>
-            <Button variant="ghost" className="h-8 w-8 p-0">
+            <div className="h-8 w-8 p-0">
                 <span className="sr-only">Open menu</span>
                 <MoreHorizontal className="h-4 w-4" />
-            </Button>
+            </div>
           </DropdownMenuTrigger>
 
           <DropdownMenuContent>
