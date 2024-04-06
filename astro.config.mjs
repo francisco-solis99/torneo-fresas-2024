bfb0579eedc1b7d6fb696aeabcbbeea33c8fb825
francisco-solis99 <franciscosoliscss@gmail.com>
2024-04-06 12:48:22 -0600

    [build] change the node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,7 +15,5 @@ export default defineConfig({
       exclude: ["oslo"]
     }
   },
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel()
 });
