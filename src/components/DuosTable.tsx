ccfbe14dd5fdbb997f19eaf3a3e6a3bbd0906898
francisco-solis99 <franciscosoliscss@gmail.com>
2024-04-06 12:12:02 -0600

    [fix] genarate matches

--- src/components/DuosTable.tsx
+++ src/components/DuosTable.tsx
@@ -23,9 +23,9 @@ import {
 import { Button } from '@/components/ui/button'
 import { Input } from "@/components/ui/input"
 import { UndoDot, Trash2 } from 'lucide-react';
+import confetti from 'canvas-confetti';
 import type React from "react"
 import { getDuos } from "@/services/duos"
-import { createMatch } from "@/services/matches"
 
 
 
@@ -49,49 +49,21 @@ export default function DuosTable(props: any) {
 
   const handlerGenerateGroupsMatches = async () => {
     const {duos: randomDuos} = await getDuos({random: true});
-    const NUM_DUOS_PER_GROUP = 3;
+    const NUM_DUOS_PER_GROUP = 4;
 
     // set groupIds in duos
-    // let indexGroup = 0;
-    // randomDuos.forEach(async (duo: any, index: number) => {
-    //   if (index % NUM_DUOS_PER_GROUP === 0) indexGroup += 1;
-    //   const groupId = groups[indexGroup - 1].id;
+    let indexGroup = 0;
+    randomDuos.forEach(async (duo: any, index: number) => {
+      if (index % NUM_DUOS_PER_GROUP === 0) indexGroup += 1;
+      const groupId = groups[indexGroup - 1].id;
 
-    //   updateDuo({idDuo: duo.id, updateData: {
-    //     player1: duo.player1,
-    //     player2: duo.player2,
-    //     group_id: groupId,
-    //   }})
-    // });
-
-    // create the matches for each group
-    // groups.forEach(group => {
-    //   const duosByGroup = randomDuos.filter((duo: any) => duo.groupId === group.id)
-    //   if(duosByGroup.length === 3) {
-    //     const [duo1, duo2, duo3] = duosByGroup;
-    //     createMatch({
-    //       duo1_id: duo1.id,
-    //       duo2_id: duo2.id,
-    //       points_d1: 0,
-    //       points_d2: 0,
-    //       phase_id: 1
-    //     })
-    //     createMatch({
-    //       duo1_id: duo1.id,
-    //       duo2_id: duo3.id,
-    //       points_d1: 0,
-    //       points_d2: 0,
-    //       phase_id: 1
-    //     })
-    //     createMatch({
-    //       duo1_id: duo2.id,
-    //       duo2_id: duo3.id,
-    //       points_d1: 0,
-    //       points_d2: 0,
-    //       phase_id: 1
-    //     })
-    //   }
-    // })
+      updateDuo({idDuo: duo.id, updateData: {
+        player1: duo.player1,
+        player2: duo.player2,
+        group_id: groupId,
+      }})
+    });
+    confetti()
   }
 
   return (
@@ -105,7 +77,7 @@ export default function DuosTable(props: any) {
         )
         : (
           <>
-            <Button size={"sm"} className={`mt-8 mb-3`} onClick={handlerGenerateGroupsMatches}>Generar grupos y matches</Button>
+            <Button size={"sm"} className={`mt-8 mb-3`} onClick={handlerGenerateGroupsMatches}>Generar grupos</Button>
             <Table className="overflow-x-auto">
               <TableCaption>Lista de parejas para el torneo de las fresas</TableCaption>
               <TableHeader>
